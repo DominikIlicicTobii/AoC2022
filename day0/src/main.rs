c664cbe5c784add0377601e254c4e5d1662b4387
Dominik Ilicic <dominik.ilicic@tobii.com>
2022-11-30 13:42:14 +0100

    Revert "day0: solved"
    This reverts commit bb81b7f28029729c44b00ddad8d457fbbade4e11.

--- day0/src/main.rs
+++ day0/src/main.rs
@@ -1,13 +1,3 @@
-use std::fs::File;
-
-use day0::*;
-
 fn main() {
-    let measurements = read_measurements(File::open("input/measurements.txt").unwrap());
-
-    // answer 1
-    assert_eq!(num_of_increases(&measurements), 1521);
-
-    // answer 2
-    assert_eq!(num_of_increases_on_windows(&measurements), 1543);
+    println!("Hello, world!");
 }
